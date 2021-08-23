64fb98af052ab73c0d8d52ef217b70ed4f2c6ebe
nikhil3456 <nikhilbansal3456@gmail.com>
2021-08-23 15:27:14 +0530

    add sys path

--- tools/visualize_svg_data.py
+++ tools/visualize_svg_data.py
@@ -1,6 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
+
 import numpy as np
 import tensorflow as tf
-import os
 import matplotlib.pyplot as plt
 from matplotlib.patches import Polygon
 import glob
